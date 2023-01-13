e7ad9026dccf65d155e8fc18062d19820db87b1b
Kleverson Costa <kleverson.mmc@gmail.com>
2023-01-13 18:07:10 -0300

    implements signUp and SignIn

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,11 +8,11 @@ export class AuthController {
 
     @Post('signup')
     signup(@Body() dto: AuthDTO) {
-        return this.authService.signup()
+        return this.authService.signup(dto)
     }
 
     @Post('signin')
-    signin() {
-        return this.authService.signin()
+    signin(@Body() dto: AuthDTO) {
+        return this.authService.signin(dto)
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,41 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import * as argon2 from "argon2";
+import { AuthDTO } from "./dto";
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService) {}
-    signup() {
-        return 'Sign Up service'
+    constructor(private prisma: PrismaService) { }
+    async signup(dto: AuthDTO) {
+        const hash = await argon2.hash(dto.password)
+        const user = await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                hash: hash
+            }
+        })
+        delete user.hash
+        return user
     }
 
-    signin() {
-        return 'Sign In service'
+    async signin(dto: AuthDTO) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email
+            }
+        })
+
+        if (!user)
+            throw new ForbiddenException('Credentials incorrect')
+
+        const passwordsMatch = await argon2.verify(
+            user.hash,
+            dto.password)
+
+        if (!passwordsMatch)
+            throw new ForbiddenException('Credentials incorrect')
+
+        delete user.hash
+        return user
     }
 }
